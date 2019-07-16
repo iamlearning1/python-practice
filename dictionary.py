51309927857c7be48f58532102c2fed270d95f18
Deepak Kumar <d1612k@gmail.com>
2019-07-16 22:15:10 +0530

    added get best matches

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 with open('data.json') as file:
     data = json.load(file)
@@ -9,11 +10,12 @@ choice = 'Y'
 while choice is 'Y':
     try:
         key = (input('Enter a word to get it\'s definition: ')).lower()
+        key = get_close_matches(key, data.keys(), n=1, cutoff=0.6)
         count = 1
-        for value in data[key]:
+        for value in data[key[0]]:
             print(f'{count}. {value}')
             count += 1
         choice = input('Do you want to continue ? (Y/N) ')
-    except KeyError:
+    except IndexError:
         print('Choose another word')
         continue
