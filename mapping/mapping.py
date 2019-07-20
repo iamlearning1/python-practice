e88fc19b2b58a7c04cc52ce67a347c0fdbef9c3e
Deepak Kumar <d1612k@gmail.com>
2019-07-20 14:20:36 +0530

    added a marker

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -1,4 +1,7 @@
 import folium
 
-map = folium.Map(location=[80, -100])
+map = folium.Map(location=[38.58, -99.09])
+
+map.add_child(folium.Marker(
+    location=[38.5, -99], popup="marker", icon=folium.Icon(color='green')))
 map.save('map1.html')
